76998391eaab4547052021393c7f2a17d5ab2c17
Moshe Berman <moshberm@gmail.com>
2014-12-14 16:48:29 -0500

    Integrate MyoKit.

--- Myonnaistick/AppDelegate.m
+++ Myonnaistick/AppDelegate.m
@@ -17,6 +17,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    /**
+     *  MyKit support
+     */
+    
+    [TLMHub sharedHub];
+    
     return YES;
 }
 
